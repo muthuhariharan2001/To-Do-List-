094f230ce627681158e7cde72763bbeec55de022
muthuhariharan2001 <164756456+muthuhariharan2001@users.noreply.github.com>
2024-03-24 19:12:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const taskInput = document.getElementById('taskInput');
+    const addTaskBtn = document.getElementById('addTaskBtn');
+    const taskList = document.getElementById('taskList');
+  
+    addTaskBtn.addEventListener('click', function() {
+      const taskText = taskInput.value.trim();
+      if (taskText !== '') {
+        addTask(taskText);
+        taskInput.value = '';
+      }
+    });
+  
+    function addTask(taskText) {
+      const li = document.createElement('li');
+      li.textContent = taskText;
+      taskList.appendChild(li);
+    }
+  });
+  
